6f29c0374b82ad7085055aea28694367dc0693c9
body <abdo.mhamed122yahoo.com@gmail.com>
2024-12-13 17:55:13 +0200

    make heritage section responsive on mobile

--- components/Heritage.jsx
+++ components/Heritage.jsx
@@ -10,7 +10,7 @@ const Heritage = ({ translations }) => {
       />
       <section className="text-white bg-[#544229cb] px-4 md:px-28 pt-10 md:pt-20 pb-10 md:pb-16 z-20 relative -mt-7">
         <div className="flex flex-col md:flex-row items-start gap-6 md:gap-20">
-          <div className="w-full relative order-2 md:order-1">
+          <div className="w-full relative order-2 md:order-1 max-md:hidden">
             <img
               src="/images/Artboard_20-8.png"
               className="w-full relative"
@@ -21,47 +21,42 @@ const Heritage = ({ translations }) => {
               className="absolute bottom-0 rounded-xl w-full z-20 object-contain md:max-w-none max-w-40 max-md:hidden"
             />
           </div>
-          <div className="text-[#FFFFFF] flex flex-col justify-end items-end w-full order-1 md:order-2">
-            <h1 className="text-2xl md:text-3xl mb-4 md:mb-6 t w-full">
+          <div className="text-[#FFFFFF] flex flex-col justify-end items-start w-full order-1 md:order-2">
+            <h1 className="text-2xl md:text-3xl mb-4 md:mb-6 t w-full max-sm:text-xl">
               {translations.heritage.title}
             </h1>
-            <p className="mb-6 md:mb-12 t">
+            <p className="mb-6 md:mb-12 max-sm:text-sm">
               {translations.heritage.description1}
             </p>
-            <div className="w-full relative">
-              <img
-                src="/images/hFlag_3_1.png"
-                className="w-full relative"
-                alt="Screenshot"
-              />
-              <img
-                src="/images/Rectangle_464_(1).png"
-                className="absolute bottom-0 rounded-xl w-full z-20 object-contain md:max-w-none max-sm:hidden max-w-40 max-md:hidden"
-              />
+            <div className=" max-md:flex  max-md:flex-row-reverse  max-md:gap-8 ">
+              <div className="w-full relative  max-md:mt-24">
+                <img
+                  src="/images/hFlag_3_1.png"
+                  className="w-full relative"
+                  alt="Screenshot"
+                />
+                <img
+                  src="/images/Rectangle_464_(1).png"
+                  className="absolute bottom-0 rounded-xl w-full z-20 object-contain md:max-w-none max-sm:hidden max-w-40 max-md:hidden"
+                />
+              </div>
+              <div className="w-full relative order-2 md:order-1 md:hidden">
+                <img
+                  src="/images/Artboard_20-8.png"
+                  className="w-full relative"
+                  alt="Screenshot"
+                />
+                <img
+                  src="/images/Rectangle_464_(1).png"
+                  className="absolute bottom-0 rounded-xl w-full z-20 object-contain md:max-w-none max-w-40 max-md:hidden"
+                />
+              </div>
             </div>
           </div>
         </div>
-        <div className="flex flex-col md:flex-row items-start gap-6 md:gap-20 mt-10 md:mt-36">
-          <div className="text-[#FFFFFF] flex flex-col justify-end w-full order-2 md:order-1">
-            <h1 className="text-2xl md:text-3xl mb-4 md:mb-6  w-full">
-              {translations.heritage.title2}
-            </h1>
-            <p className="mb-6 md:mb-12 ">
-              {translations.heritage.description2}
-            </p>
-            <div className="w-full relative">
-              <img
-                src="/images/hFlag.png"
-                className="w-full relative object-cover"
-                alt="Screenshot"
-              />
-              <img
-                src="/images/Rectangle_464_(1).png"
-                className="absolute bottom-0 rounded-xl w-full z-20 object-contain md:max-w-none max-w-40 max-md:hidden"
-              />
-            </div>
-          </div>
-          <div className="w-full relative order-1 md:order-2">
+
+        <div className="flex flex-col md:flex-row-reverse  items-start gap-6 md:gap-20 mt-12   md:mt-32">
+          <div className="w-full relative order-2 md:order-1 max-md:hidden">
             <img
               src="/images/dessert.png"
               className="w-full relative"
@@ -69,8 +64,40 @@ const Heritage = ({ translations }) => {
             />
             <img
               src="/images/Rectangle_464_(1).png"
-              className="absolute bottom-0 rounded-xl w-full z-20 object-contain md:max-w-none max-w-40 max-md:hidden "
+              className="absolute bottom-0 rounded-xl w-full z-20 object-contain md:max-w-none max-w-40 max-md:hidden"
             />
+          </div>
+          <div className="text-[#FFFFFF] flex flex-col justify-end items-start w-full order-1 md:order-2">
+            <h1 className="text-2xl md:text-3xl mb-4 md:mb-6 t w-full max-sm:text-xl">
+              {translations.heritage.title2}
+            </h1>
+            <p className="mb-6 md:mb-12 max-sm:text-sm">
+              {translations.heritage.description2}
+            </p>
+            <div className=" max-md:flex  max-md:flex-row  max-md:gap-8 ">
+              <div className="w-full relative  max-md:mt-24">
+                <img
+                  src="/images/hFlag.png"
+                  className="w-full relative  max-sm:h-36 max-md:h-44  lg:h-96"
+                  alt="Screenshot"
+                />
+                <img
+                  src="/images/Rectangle_464_(1).png"
+                  className="absolute bottom-0 rounded-xl w-full z-20 object-contain md:max-w-none max-sm:hidden max-w-40 max-md:hidden"
+                />
+              </div>
+              <div className="w-full relative order-2 md:order-1 md:hidden">
+                <img
+                  src="/images/dessert.png"
+                  className="w-full relative"
+                  alt="Screenshot"
+                />
+                <img
+                  src="/images/Rectangle_464_(1).png"
+                  className="absolute bottom-0 rounded-xl w-full z-20 object-contain md:max-w-none  max-md:hidden"
+                />
+              </div>
+            </div>
           </div>
         </div>
       </section>
